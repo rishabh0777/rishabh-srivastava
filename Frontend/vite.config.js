f7e668da67b55ea8fcd84588ff2279c6a58cf45f
Rishabh Srivastava <104848594+rishabh0777@users.noreply.github.com>
2025-02-04 18:57:03 +0000

    created 404.html page and configured vite.config.js file

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    historyApiFallback: true, // Redirects unknown routes to index.html
+  },
+});
